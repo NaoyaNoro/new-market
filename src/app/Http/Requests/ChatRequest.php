0ef0126601f65bbbdff68ed325aa81493bd374c1
naoya noro <naoya20180830@gmail.com>
2025-08-26 10:04:10 +0900

    リクエストの修正

--- src/app/Http/Requests/ChatRequest.php
+++ src/app/Http/Requests/ChatRequest.php
@@ -33,7 +33,7 @@ class ChatRequest extends FormRequest
     {
         return [
             'message.required' => '本文を入力してください',
-            'message.max' => '本文は400字以内で入力してください',
+            'message.max' => '本文は400文字以内で入力してください',
             'image.mimes' => '「.png」または「.jpeg」形式でアップロードしてください',
         ];
     }
